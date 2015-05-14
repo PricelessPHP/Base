7fc12788626db68eff116f736bd53fa280696206
MarQuis Knox <MarQuisKnox@users.noreply.github.com>
2015-05-14 12:48:57 +0200

    Update truncate.sql

--- scripts/destroy/db/truncate.sql
+++ scripts/destroy/db/truncate.sql
@@ -1,6 +1,6 @@
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE `cloneui_phrase`, `cloneui_site_config`, `cloneui_site_theme`, `cloneui_usergroup_member`, `cloneui_usergroup_permission`, `cloneui_user_confirm`, `cloneui_usergroup`, `cloneui_site_permission`, `cloneui_banned_email`, `cloneui_banned_ip`, `cloneui_user`, `cloneui_language`;  
-DROP TABLE `cloneui_metadata` ;
-DROP TABLE `cloneui_oauth_token` ;
-DROP TABLE `cloneui_oauth_app` ;
+DROP TABLE `base_phrase`, `base_site_config`, `base_site_theme`, `base_usergroup_member`, `base_usergroup_permission`, `base_user_confirm`, `base_usergroup`, `base_site_permission`, `base_banned_email`, `base_banned_ip`, `base_user`, `base_language`;  
+DROP TABLE `base_metadata` ;
+DROP TABLE `base_oauth_token` ;
+DROP TABLE `base_oauth_app` ;
 SET FOREIGN_KEY_CHECKS = 1;
