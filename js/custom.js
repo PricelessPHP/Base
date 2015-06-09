ae5a739b5081a2c601c8ceefd77e78dcb5bd4490
MarQuis Knox <MarQuisKnox@users.noreply.github.com>
2015-06-09 13:32:09 +0200

    Update custom.js
    ADDED:  Date.prototype.getUnixTime()

--- js/custom.js
+++ js/custom.js
@@ -644,3 +644,8 @@ if(!Array.indexOf) {
         return -1;
     };
 }
+
+// @link	ttps://coderwall.com/p/rbfl6g/how-to-get-the-correct-unix-timestamp-from-any-date-in-javascript
+Date.prototype.getUnixTime = function() { return this.getTime()/1000|0 };
+if(!Date.now) Date.now = function() { return new Date(); }
+Date.time = function() { return Date.now().getUnixTime(); }
